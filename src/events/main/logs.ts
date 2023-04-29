0f523f56d770e0c991e43da552ca6f75a674a18b
Peterson Larson <peter-larson@hotmail.com>
2023-04-29 19:38:15 -0300

    feature/mention_command

--- src/events/main/logs.ts
+++ src/events/main/logs.ts
@@ -4,6 +4,8 @@ export default new Event({
     name: "interactionCreate",
     once: true,
     run(interaction) {
-        console.log(`\nInteraction Listener`.yellow,`\nCommand: ${interaction} \nAuthor: ${interaction.user.tag}\nAuthor Id: ${interaction.user.id}`.magenta);
+        if (interaction.isCommand() || interaction.isUserContextMenuCommand()){
+            console.log(`\nInteraction Listener`.yellow,`\nCommand: ${interaction.commandName} \nAuthor: ${interaction.user.tag}\nAuthor Id: ${interaction.user.id}`.magenta);
+        }
     }
 });
