eba9994d23f2046da8240115e721fae073d61366
Pscodium <peter-larson@hotmail.com>
2023-12-20 14:50:35 -0300

    feature/api_initialization

--- app/index.ts
+++ app/index.ts
@@ -2,7 +2,6 @@ import { ExtendedClient } from './structs/ExtendedClient';
 export * from 'colors';
 import * as config from './config.json';
 import { sequelize } from './data-source';
-import { api } from '../api/index';
 const client = new ExtendedClient();
 
 client.start();
@@ -10,7 +9,6 @@ client.start();
 sequelize.authenticate()
     .then(() => {
         console.log("Connection established!");
-        api.start();
     })
     .catch((err) => {
         console.error("Error authenticating database: ", err);
